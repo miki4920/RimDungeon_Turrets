b9d8e1bba98486b267538fcc7c1ad65e53c0d3b1
Nicolas Grobelny <miki4920@gmail.com>
2021-08-27 19:20:29 +0100

    Added a working shotgun style multishot

--- Source/Turret_Def.cs
+++ Source/Turret_Def.cs
@@ -10,7 +10,23 @@ namespace RimDungeon
 {
     public class Turret_Def : DefModExtension
     {
+
+        private static readonly Turret_Def DefaultValues = new Turret_Def();
+
+        public static Turret_Def Get(Def def)
+        {
+            return def.GetModExtension<Turret_Def>() ?? DefaultValues;
+        }
+
         public List<ThingDef> guns = null;
+        public float firingArc = 360;
+
+        
+    }
+
+    public class Projectile_Def : DefModExtension
+    {
+        public int shotCount = 1;
     }
 }
 
